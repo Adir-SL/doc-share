ba861a2a3314f413a2600eb40a9c59c35fe0dae3
Adir <slutzkis@gmail.com>
2025-05-05 15:01:50 +0300

    updates

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function saveFunc() {
     window.location.hash = document.getElementById("docInput").innerText;
 }
+
+function loadFunc() {
+    document.getElementById("docInput").innerText = window.location.hash;
+}

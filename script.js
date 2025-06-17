212e465f050651baa4d828fdf05bd61bf2c0944a
Adir <slutzkis@gmail.com>
2025-06-17 14:53:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function saveFunc() {
 }
 
 function loadFunc() {
-    document.getElementById("docInput").innerText = window.location.hash.slice(1, window.location.hash.length);
+    document.getElementById("docInput").innerText = decodeURIComponent(window.location.hash.slice(1, window.location.hash.length));
 }

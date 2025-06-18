ea6bf75f1676c520c6c95a34e1f4eee558dc0d92
Adir <slutzkis@gmail.com>
2025-06-18 18:06:23 +0300

    updates

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function saveFunc() {
-    // window.location.hash = document.getElementById("docInput").innerText.replace(/\n/g, '%0A');
     window.location.hash = encodeURIComponent(document.getElementById("docInput").innerText);
 }
 
